48a5247e153270864f423b59830d0633ee52cb5f
Grigory Gerasimov <grigorygerasimov@Grigorys-MacBook-Pro.local>
2023-07-03 09:26:05 +0200

    basic components init

---
+++ resources/js/components/Auth/Register.vue
@@ -0,0 +1,24 @@
+<script>
+import { defineComponent } from 'vue'
+import { Button, FormControl } from '../Common'
+
+export default defineComponent({
+    name: 'Register',
+    components: {
+        FormControl,
+        Button
+    }
+})
+</script>
+
+<template>
+    <FormControl id='' title='' type='' name=''/>
+    <FormControl id='' title='' type='' name=''/>
+    <FormControl id='' title='' type='' name=''/>
+    <FormControl id='' title='' type='' name=''/>
+    <Button type=''/>
+</template>
+
+<style scoped>
+
+</style>
