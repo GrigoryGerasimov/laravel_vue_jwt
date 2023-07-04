4d1983d80517824b0cde3c941ec72f001e20b932
Grigory Gerasimov <grigorygerasimov@Grigorys-MacBook-Pro.local>
2023-07-04 09:22:09 +0200

    user login implemented, user signin controller refactored, user signin request added, signin component upd

--- app/Http/Controllers/User/Auth/SignInController.php
+++ app/Http/Controllers/User/Auth/SignInController.php
@@ -3,10 +3,18 @@
 namespace App\Http\Controllers\User\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\Auth\SignInRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
 
 class SignInController extends Controller
 {
-    public function __invoke()
-    {}
+    public function __invoke(SignInRequest $request): ?UserResource
+    {
+        $loginData = $request->validated();
+
+        $user = User::where(['email' => $loginData['email']])->first();
+
+        return password_verify($loginData['password'], $user->password) ? new UserResource($user) : null;
+    }
 }
--- resources/js/components/Auth/SignIn.vue
+++ resources/js/components/Auth/SignIn.vue
@@ -8,6 +8,29 @@ export default defineComponent({
     components: {
         FormControl,
         Button
+    },
+
+    data() {
+        return {
+            email: null,
+            password: null
+        }
+    },
+
+    computed: {
+        login() {
+            return {
+                email: this.email,
+                password: this.password
+            }
+        }
+    },
+
+    methods: {
+        async submitHandler() {
+            const response = await axios.post('/api/user/auth/signin', this.login);
+            console.log(response.data)
+        }
     }
 })
 </script>
@@ -20,6 +43,7 @@ export default defineComponent({
             input-type='email'
             input-name='email'
             input-placeholder='Email'
+            v-model='email'
         />
         <FormControl
             input-id='password'
@@ -27,8 +51,9 @@ export default defineComponent({
             input-type='password'
             input-name='password'
             input-placeholder='Password'
+            v-model='password'
         />
-        <Button btn-type='submit' class='btn-outline-success'>Submit</Button>
+        <Button btn-type='submit' class='btn-outline-success' @click.prevent='submitHandler'>Submit</Button>
     </div>
 </template>
 
